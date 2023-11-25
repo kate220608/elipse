6cb12bcee36a7d032ea71f09bfff1936c09d2d77
kate220608 <katyfar2008@mail.ru>
2023-11-25 11:22:26 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_elipse(qp)
+            qp.end()
+
+    def draw_elipse(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        n = random.randrange(0, 100)
+        for i in range(n):
+            d = random.randrange(0, 200)
+            y = random.randrange(0, 600)
+            x = random.randrange(0, 800)
+            qp.drawEllipse(x, y, d, d)
+        self.do_paint = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
